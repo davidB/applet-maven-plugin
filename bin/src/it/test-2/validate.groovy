941d41a3a49b130702a47c2a3306d912d53eb4d8
David Bernard <david.bernard.31@gmail.com>
2009-04-25 17:23:44 +0200

    first clean

--- bin/src/it/test-2/validate.groovy
+++
@@ -1,7 +0,0 @@
-def jnlp = new File(basedir, 'target/jws/launch.jnlp')
-assert jnlp.exists()
-jnlp.eachLine { line -> assert line.indexOf('${') < 0 : "non evaluated value in :" + line }
-
-def html = new File(basedir, 'target/jws/run.html')
-assert html.exists()
-html.eachLine { line -> assert line.indexOf('${') < 0 : "non evaluated value in :" + line }
